99f560533c3e706776df5a4bcc617ce29cfc2519
AntoineChauvel <antoine.chauvel@viacesi.fr>
2019-11-08 15:57:28 +0100

    Event main page and single page.

--- phpServer/resources/views/events/singleEvent.blade.php
+++ phpServer/resources/views/events/singleEvent.blade.php
@@ -8,24 +8,39 @@
     Le bureau des élèves de Saint-Nazaire organise une nouvelle activité, c'est {{ $event->name }}, qui consiste à {{ $event->description }}
 @endsection
 
+@push('stylesheet')
+    <link href="{{ asset('css/project-css/events.css') }}" rel="stylesheet">
+@endpush
+
 
 @section('content')
 
-    <article class="eventPresentation">
+    <h1 id="singleEventTitle">{{$event->name}}</h1>
 
-          <img src="https://via.placeholder.com/300">
-        <div class="row">
-            <h1>{{$event->name}}</h1>
+    <article class="eventPresentation row mx-auto">
 
+        <div class="col-md">
+            <img src="https://via.placeholder.com/300" class="eventMainImage">
         </div>
 
+        <div class="col-md">
+            <div class="row">
+                <p class="col-md ">Date de l'évènement : {{$event->date}}</p>
+                <p class="col-md ">Lieu : {{$event->location}}</p>
+            </div>
+
+            <div class="row">
+                <div clas="eventInformations">
+                    <p id="eventDescription" class="col-md">{{$event->description}}</p>
+                    <div class="row justify-content-center">
+                        <a href="#" class="btn btn-dark">Participer à l'évènement</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
     </article>
-
-
-
-
-
-
-
 
 @endsection
