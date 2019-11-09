ab3aa73ff0fd753ae881ddd9aea00ed1d8fe83b4
AntoineChauvel <antoine.chauvel@viacesi.fr>
2019-11-09 19:52:52 +0100

    Events handling (add/edit) + views modified.

--- phpServer/app/Event.php
+++ phpServer/app/Event.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'description', 'location', 'date', 'price'
+    ];
+
+    public function image()
+    {
+        return $this->hasOne(Images::class, 'image_id');
+    }
 }
---
+++ phpServer/resources/views/partials/events/_form.blade.php
@@ -0,0 +1,48 @@
+<div class="form-group">
+    <label for="name">Nom de l'évènement :</label>
+    <input type="text" name="name" id="name" value="@isset($event) {{$event->name }} @endisset {{ old('name') }}" class="form-control" required>
+    @error('name')
+    <p>{{ $errors->first('name') }}</p>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="description">Description de l'évènement :</label>
+    <textarea name="description" id="description"  class="form-control"
+              required>@isset($event) {{$event->description }} @endisset {{ old('description') }}</textarea>
+    @error('description')
+    <p>{{ $errors->first('description') }}</p>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="location">Lieu de l'évènement :</label>
+    <input type="text" name="location" id="location" value="@isset($event) {{$event->location }} @endisset {{ old('location') }}" class="form-control" required>
+    @error('location')
+    <p>{{ $errors->first('location') }}</p>
+    @enderror
+</div>
+
+<div class="custom-file">
+    <input type="file" class="custom-file-input form-control" id="image" name="image">
+    <label class="custom-file-label" for="image">Choisir une image pour l'évènement</label>
+</div>
+
+
+<div class="form-group">
+    <label for="date">Date de l'évènement :</label>
+    <input type="date" name="date" id="date" value="{{ old('date', date('Y-m-d')) }}" class="form-control" required>
+    @error('date')
+    <p>{{ $errors->first('date') }}</p>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="price">Prix l'évènement (facultatif) :</label>
+    <input type="number" name="price" id="price" value="@isset($event) {{$event->price }} @endisset {{ old('price') }}" class="form-control" min="0" max="15">
+    @error('price')
+    <p>{{ $errors->first('price') }}</p>
+    @enderror
+</div>
+
+
