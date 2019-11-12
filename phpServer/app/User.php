0843a8ef02de6fbd88859dca7f78773931d22a9e
TomBANCHEREAU <banchereau.tom@gmail.com>
2019-11-12 09:02:07 +0100

    Creating a personalized model for the user

--- phpServer/app/User.php
+++ phpServer/app/User.php
@@ -2,38 +2,59 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use GuzzleHttp\Client as HTTPClient;
+use GuzzleHttp\Psr7\Request as HTTPRequest;
 
-class User extends Authenticatable
+
+class User
 {
-    use Notifiable;
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $campus;
+    public $status;
+    public $statusLvl;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    public function __constructor($user = NULL)
+    {
+        if ($user != NULL) {
+            $this->id = $user->id;
+            $this->firstname = $user->firstname;
+            $this->lastname = $user->lastname;
+            $this->email = $user->email;
+            $this->campus = $user->campus;
+            $this->status = $user->status;
+            $this->statusLvl = $user->statusLvl;
+        }
+    }
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public static function find($id)
+    {
+        $client = new HTTPClient();
+        $httpRequest = new HTTPRequest('get', 'http://' . env('ACCOUNT_SERVER_IP') . '/users/' . $id);
+        $response = json_decode($client->send($httpRequest)->getBody()->getContents());
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+        if ($response->status == 'success') {
+            $user = new User($response->result);
+            if ($user->id != NULL) {
+                return $user;
+            }
+        }
+        return NULL;
+    }
+
+    public static function all()
+    {
+        $client = new HTTPClient();
+        $httpRequest = new HTTPRequest('get', 'http://' . env('ACCOUNT_SERVER_IP') . '/users');
+        $response = json_decode($client->send($httpRequest)->getBody()->getContents());
+        $users = [];
+        if ($response->status == 'success') {
+            foreach($response->result as $user){
+                $users[] = new User($user);
+            }
+        }
+        return $users;
+    }
 }
