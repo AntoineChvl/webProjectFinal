62ac9fba38475808f5a163851435a2fafa606365
TomBANCHEREAU <banchereau.tom@gmail.com>
2019-11-13 13:20:04 +0100

    Modifying User Model

--- phpServer/app/User.php
+++ phpServer/app/User.php
@@ -16,7 +16,7 @@ class User
     public $status;
     public $statusLvl;
 
-    public function __constructor($user = NULL)
+    public function __construct($user = NULL)
     {
         if ($user != NULL) {
             $this->id = $user->id;
@@ -58,6 +58,7 @@ class User
     public static function auth()
     {
         if (session()->has('authenticated')) {
+            session()->put('authenticated',static::find(session()->get('authenticated')->id));
             return session()->get('authenticated');
         }
         return NULL;
